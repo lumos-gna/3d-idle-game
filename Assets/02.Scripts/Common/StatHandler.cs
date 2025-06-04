c75a0c4c6cbb83ffa2f1a666af7babff828ec554
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-06-04 22:46:04 +0900

    feat : 동적 맵 생성 구현

--- Assets/02.Scripts/Common/StatHandler.cs
+++ Assets/02.Scripts/Common/StatHandler.cs
@@ -1,18 +1,37 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StatHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private StatData statData;
+
+    private readonly Dictionary<StatType, float> _statDict = new();
+
+    private void Awake()
     {
-        
+        InitializeStat();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitializeStat()
     {
-        
+        foreach (StatEntry entry in statData.Stats)
+        {
+            _statDict[entry.type] = entry.value;
+        }
     }
+    
+    public float GetStat(StatType statType)
+    {
+        return _statDict.ContainsKey(statType) ? _statDict[statType] : 0;
+    }
+
+    public void ModifyStat(StatType statType, float amount)
+    {
+        if (!_statDict.ContainsKey(statType)) return;
+
+        _statDict[statType] += amount;
+    }
+    
+    
 }
--- Assets/02.Scripts/StageManager.cs
+++ Assets/02.Scripts/StageManager.cs
@@ -1,8 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class StageManager : MonoBehaviour
 {
+   [SerializeField] private GameObject roomPrefab;
+   [SerializeField] private GameObject pathPrefab;
+
+   [SerializeField] private int maxRoomCount;
    
+   [SerializeField] private int roomLength;
+   [SerializeField] private int pathLength;
+   
+   private void Start()
+   {
+      List<Vector2Int> roomTree = CreateRoomTree();
+
+      CreateRooms(roomTree);
+      CreatePath(roomTree);
+   }
+
+   private List<Vector2Int> CreateRoomTree()
+   {
+      List<Vector2Int> newTree = new()
+      {
+         Vector2Int.zero
+      };
+      
+      Vector2Int[] directions4 = new[]
+      {
+         Vector2Int.down,
+         Vector2Int.left,
+         Vector2Int.right,
+         Vector2Int.up
+      };
+      
+      for (int count = 0; count < maxRoomCount; count++)
+      {
+         for (int dirIndex = 0; dirIndex < directions4.Length; dirIndex++)
+         {
+            int randCount = UnityEngine.Random.Range(0, directions4.Length);
+            
+            Vector2Int targetNode =  newTree[^1] + directions4[randCount];
+
+            if (!newTree.Contains(targetNode))
+            {
+               newTree.Add(targetNode);
+
+               break;
+            }
+         }
+      }
+
+      return newTree;
+   }
+
+   void CreateRooms(List<Vector2Int> roomTree)
+   {
+      for (int i = 0; i < roomTree.Count; i++)
+      {
+         var createRoom = Instantiate(roomPrefab);
+
+         Vector3 placePos = new Vector3(roomTree[i].x, 0, roomTree[i].y) * (roomLength + pathLength);
+
+         createRoom.transform.position = placePos;
+      }
+   }
+
+   void CreatePath(List<Vector2Int> roomTree)
+   {
+      for (int i = 0; i < roomTree.Count - 1; i++)
+      {
+         var createPath = Instantiate(pathPrefab);
+         
+         Vector2 centerPos = (Vector2)(roomTree[i + 1] + roomTree[i])  * 0.5f;
+
+         createPath.transform.position = new Vector3(centerPos.x, 0, centerPos.y) * (roomLength + pathLength);
+         
+         
+         Vector2 delta = roomTree[i + 1] - roomTree[i];
+         
+         if (delta.y != 0) 
+         {
+            createPath.transform.rotation = Quaternion.Euler(0, 90f, 0);
+         }
+      }
+   }
 }
